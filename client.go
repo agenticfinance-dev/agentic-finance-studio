4b9f563c235d6a21922a20628efa769a093a732e
agenticfinance <bitconsult1@gmail.com>
2026-07-16 18:43:23 +0100

    Create client.go

---
+++ client.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+
+	"github.com/sodex-tech/sodex-go-sdk-public/client"
+)
+
+type SoDEXClient struct {
+	Client *client.Client
+}
+
+func NewSoDEXClient(privateKey *ecdsa.PrivateKey) *SoDEXClient {
+	cfg := client.Config{
+		BaseURL:    client.MainnetBaseURL,
+		ChainID:    286623,
+		PrivateKey: privateKey,
+	}
+
+	return &SoDEXClient{
+		Client: client.New(cfg),
+	}
+}
+
+func (s *SoDEXClient) Health() string {
+	return "SoDEX SDK Connected"
+}
+
+func (s *SoDEXClient) GetSymbols(ctx context.Context) ([]client.Symbol, error) {
+	symbols, err := s.Client.PerpsSymbols(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return symbols, nil
+}
+
+func (s *SoDEXClient) Address() string {
+	return fmt.Sprintf("%s", s.Client.Address())
+}
