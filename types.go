1dc0884c1f089fb102ba2089d1499a7c10bc9c27
agenticfinance <bitconsult1@gmail.com>
2026-07-16 20:37:31 +0100

    Create types.go

---
+++ types.go
@@ -0,0 +1,25 @@
+package main
+
+type SignOrderRequest struct {
+	PrivateKey string `json:"privateKey"`
+
+	AccountID uint64 `json:"accountID"`
+	SymbolID  uint64 `json:"symbolID"`
+
+	Side         string `json:"side"`
+	PositionSide string `json:"positionSide"`
+
+	Price    string `json:"price"`
+	Quantity string `json:"quantity"`
+
+	ClOrdID string `json:"clOrdID"`
+}
+
+type SignOrderResponse struct {
+	Success bool   `json:"success"`
+
+	Signature string `json:"signature,omitempty"`
+	Address   string `json:"address,omitempty"`
+
+	Error string `json:"error,omitempty"`
+}
