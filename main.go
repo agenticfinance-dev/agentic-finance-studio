3c24c6619f8be6cee9ab0a6ebe3e8d36db04f1b9
agenticfinance <bitconsult1@gmail.com>
2026-07-17 12:56:33 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,46 +1,47 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"log"
-	"net/http"
-	"os"
+    "crypto/ecdsa"
+    "log"
+    "net/http"
+    "os"
 
-	"github.com/ethereum/go-ethereum/crypto"
+    "github.com/ethereum/go-ethereum/crypto"
 )
 
 func loadPrivateKey() *ecdsa.PrivateKey {
-	key := os.Getenv("SODEX_PRIVATE_KEY")
-	if key == "" {
-		log.Fatal("SODEX_PRIVATE_KEY environment variable not set")
-	}
-
-	pk, err := crypto.HexToECDSA(key)
-	if err != nil {
-		log.Fatalf("Invalid private key: %v", err)
-	}
-
-	return pk
+    key := os.Getenv("SODEX_PRIVATE_KEY")
+    if key == "" {
+        log.Fatal("SODEX_PRIVATE_KEY environment variable not set")
+    }
+    pk, err := crypto.HexToECDSA(key)
+    if err != nil {
+        log.Fatalf("Invalid private key: %v", err)
+    }
+    return pk
 }
 
 func main() {
-	privateKey := loadPrivateKey()
+    privateKey := loadPrivateKey()
 
-	sodex := NewSoDEXClient(privateKey)
+    address := crypto.PubkeyToAddress(privateKey.PublicKey).Hex()
+    log.Printf("SIGNER WALLET: %s", address)
 
-	app := &App{
-		SoDEX: sodex,
-	}
+    sodex := NewSoDEXClient(privateKey)
 
-	http.HandleFunc("/", app.HealthHandler)
-	http.HandleFunc("/symbols", app.SymbolsHandler)
-	http.HandleFunc("/sign-order", app.SignOrderHandler)
+    app := &App{
+        SoDEX: sodex,
+    }
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+    http.HandleFunc("/", app.HealthHandler)
+    http.HandleFunc("/symbols", app.SymbolsHandler)
+    http.HandleFunc("/sign-order", app.SignOrderHandler)
 
-	log.Printf("Server running on :%s", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    log.Printf("Server running on :%s", port)
+    log.Fatal(http.ListenAndServe(":"+port, nil))
 }
