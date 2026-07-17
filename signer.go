5d827bfb67f3006f51341cdaaf2d5ccf7c988829
agenticfinance <bitconsult1@gmail.com>
2026-07-17 17:56:07 +0100

    Update signer.go

--- signer.go
+++ signer.go
@@ -1,95 +1,100 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"encoding/hex"
+    "crypto/ecdsa"
+    "encoding/hex"
+    "log"
 
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/shopspring/decimal"
+    "github.com/ethereum/go-ethereum/crypto"
+    "github.com/shopspring/decimal"
 
-	"github.com/sodex-tech/sodex-go-sdk-public/common/enums"
-	perpsSigner "github.com/sodex-tech/sodex-go-sdk-public/perps/signer"
-	perpsTypes "github.com/sodex-tech/sodex-go-sdk-public/perps/types"
+    "github.com/sodex-tech/sodex-go-sdk-public/common/enums"
+    perpsSigner "github.com/sodex-tech/sodex-go-sdk-public/perps/signer"
+    perpsTypes "github.com/sodex-tech/sodex-go-sdk-public/perps/types"
 )
 
 type OrderSigner struct {
-	signer  *perpsSigner.Signer
-	address string
+    signer  *perpsSigner.Signer
+    address string
 }
 
 func NewOrderSigner(privateKey *ecdsa.PrivateKey) (*OrderSigner, error) {
-
-	s := perpsSigner.NewSigner(286623, privateKey)
-
-	address := crypto.PubkeyToAddress(privateKey.PublicKey).Hex()
-
-	return &OrderSigner{
-		signer:  s,
-		address: address,
-	}, nil
+    s := perpsSigner.NewSigner(286623, privateKey)
+    address := crypto.PubkeyToAddress(privateKey.PublicKey).Hex()
+    return &OrderSigner{
+        signer:  s,
+        address: address,
+    }, nil
 }
 
 func (o *OrderSigner) Address() string {
-	return o.address
+    return o.address
 }
 
 func (o *OrderSigner) Sign(req SignOrderRequest) (string, error) {
+    log.Println("========== GO SIGNER ==========")
+    log.Printf("AccountID: %d", req.AccountID)
+    log.Printf("SymbolID : %d", req.SymbolID)
+    log.Printf("Nonce    : %d", req.Nonce)
+    log.Printf("Price    : %s", req.Price)
+    log.Printf("Quantity : %s", req.Quantity)
+    log.Printf("Side     : %s", req.Side)
+    log.Printf("PosSide  : %s", req.PositionSide)
+    log.Println("===============================")
 
-	price, err := decimal.NewFromString(req.Price)
-	if err != nil {
-		return "", err
-	}
+    price, err := decimal.NewFromString(req.Price)
+    if err != nil {
+        return "", err
+    }
 
-	qty, err := decimal.NewFromString(req.Quantity)
-	if err != nil {
-		return "", err
-	}
+    qty, err := decimal.NewFromString(req.Quantity)
+    if err != nil {
+        return "", err
+    }
 
-	// Convert order side
-	var side enums.OrderSide
-	switch req.Side {
-	case "BUY":
-		side = enums.OrderSideBuy
-	case "SELL":
-		side = enums.OrderSideSell
-	default:
-		side = enums.OrderSideUnknown
-	}
+    var side enums.OrderSide
+    switch req.Side {
+    case "BUY":
+        side = enums.OrderSideBuy
+    case "SELL":
+        side = enums.OrderSideSell
+    default:
+        side = enums.OrderSideUnknown
+    }
 
-	// Convert position side
-	var positionSide enums.PositionSide
-	switch req.PositionSide {
-	case "LONG":
-		positionSide = enums.PositionSideLong
-	case "SHORT":
-		positionSide = enums.PositionSideShort
-	case "BOTH":
-		positionSide = enums.PositionSideBoth
-	default:
-		positionSide = enums.PositionSideUnknown
-	}
+    var positionSide enums.PositionSide
+    switch req.PositionSide {
+    case "LONG":
+        positionSide = enums.PositionSideLong
+    case "SHORT":
+        positionSide = enums.PositionSideShort
+    case "BOTH":
+        positionSide = enums.PositionSideBoth
+    default:
+        positionSide = enums.PositionSideUnknown
+    }
 
-	order := &perpsTypes.NewOrderRequest{
-		AccountID: req.AccountID,
-		SymbolID:  req.SymbolID,
-		Orders: []*perpsTypes.RawOrder{
-			{
-				ClOrdID:      req.ClOrdID,
-				Modifier:     enums.OrderModifierNormal,
-				Side:         side,
-				Type:         enums.OrderTypeLimit,
-				TimeInForce:  enums.TimeInForceGTC,
-				Price:        &price,
-				Quantity:     &qty,
-				PositionSide: positionSide,
-			},
-		},
-	}
+    order := &perpsTypes.NewOrderRequest{
+        AccountID: req.AccountID,
+        SymbolID:  req.SymbolID,
+        Orders: []*perpsTypes.RawOrder{
+            {
+                ClOrdID:      req.ClOrdID,
+                Modifier:     enums.OrderModifierNormal,
+                Side:         side,
+                Type:         enums.OrderTypeLimit,
+                TimeInForce:  enums.TimeInForceGTC,
+                Price:        &price,
+                Quantity:     &qty,
+                PositionSide: positionSide,
+            },
+        },
+    }
 
-	sig, err := o.signer.SignNewOrderRequest(order, req.Nonce)
-	if err != nil {
-		return "", err
-	}
+    sig, err := o.signer.SignNewOrderRequest(order, req.Nonce)
+    if err != nil {
+        return "", err
+    }
 
-	return hex.EncodeToString(sig), nil
+    return hex.EncodeToString(sig), nil
 }
